ae3980315f3e0a8bae1fa7509d9bb9ff3ef997d7
virreuknow <viktor.wahrb@gmail.com>
2025-09-23 13:43:33 +0200

    Upd 2

--- bok.php
+++ bok.php
@@ -1,1 +1,11 @@
 <?php 
+    if ($_SESSION[REQUEST_METHOD] == "POST") {
+        
+        $name = $_POST["name"];
+        $message = $_POST["message"];
+
+        $data = array();
+        if (file_exists("data.json")) {
+            $data = json_decode(file_get_contents('data.json'), true);
+        }
+    }
