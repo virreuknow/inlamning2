23466478aad634848b6acae19dcba5ac8397638a
virreuknow <viktor.wahrb@gmail.com>
2025-09-30 13:29:23 +0200

    something

--- bok.php
+++ bok.php
@@ -1,5 +1,5 @@
 <?php 
-    if ($_SESSION[REQUEST_METHOD] == "POST") {
+    if ($_SERVER[REQUEST_METHOD] == "POST") {
         
         $name = $_POST["name"];
         $message = $_POST["message"];
@@ -17,3 +17,25 @@
 
 ?>
 
+<div class="entry">
+    <h1>Gästbok</h1>
+    <form action="" method="POST">
+        <input class="box" type="text" name="name" placeholder="Namn"><br>
+        <input class="button" type="button" placeholder="Skicka"><br>
+    </form>
+    <div>
+        <h2>Comments</h2>
+        <div id="comments">
+            <?php
+            if (!empty($data)) {
+                foreach ($data as $item) {
+                    echo '<div class="comment">';
+                    echo '<p><strong>' . htmlspecialchars($item['name']) . '</strong>: ' . htmlspecialchars($item['message']) . '</p>';
+                    if (!empty($item['rname']) || !empty($item['rmessage'])) {
+                        echo '<div class="reply">';
+                        echo '<p><strong>' . htmlspecialchars($item['rname']) . '</strong>: ' . htmlspecialchars($item['rmessage']) . '</p>';
+                        echo '</div>';
+                    }
+                    echo '</div>';
+                }
+            } 
